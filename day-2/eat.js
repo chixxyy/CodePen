1da238f8659f8b1ef0185a1d172ee022d9312a05
chi <a27976566@gmail.com>
2024-06-28 12:23:47 +0800

    daytwo 1.1

--- day-2/eat.js
+++ day-2/eat.js
@@ -1,26 +1,19 @@
-function printChristmasTree(height) {
-    for (let i = 0; i < height; i++) {
-        let str = '';
-        for (let j = 0; j < height - i - 1; j++) {
-            str += ' '; // 在每行的前面添加空格
-        }
-        for (let k = 0; k < 2 * i + 1; k++) {
-            str += '*'; // 在每行的中間添加星號
-        }
-        console.log(str); // 打印每行
-    }
+function bimmath(tall , fat) {
+    let t=tall / 100
+    let bmi=fat / (t*t)
+    return Math.round(bmi)
+}
+console.log(bimmath(180,60));
 
-    // 打印樹幹
-    for (let i = 0; i < 2; i++) {
-        let trunk = '';
-        for (let j = 0; j < height - 1; j++) {
-            trunk += ' '; // 在樹幹前面添加空格
-        }
-        trunk += '*'; // 樹幹的星號
-        console.log(trunk);
-    }
+function hi(a) {
+    a()()
 }
 
-// 調用函數並指定聖誕樹的高度
-printChristmasTree(7);
+function hey(){
+    function inner(){
+        console.log("inner!!");
+    }
+    return inner
+}
 
+ hi(hey)
