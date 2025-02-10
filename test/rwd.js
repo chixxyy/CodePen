41f6f65c80e473f6d6f8806dc6af58c3562a6282
q_tw_daniel <danielchixxyy@icloud.com>
2025-02-10 20:40:47 +0800

    delete something

--- test/rwd.js
+++
@@ -1,9 +0,0 @@
-const car = document.querySelector('.container')
-const image = car.querySelector('card')
-let curr = 0;
-
-setInterval(() => {
-    image[curr].style.display ='none'
-    curr = (curr + 1) % image.length;
-    image[curr].style.display ='block';
-},3000);
