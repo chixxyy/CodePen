40ccd9755f078975c8d7c2af952b5e9cc4b39ef2
q_tw_daniel <danielchixxyy@icloud.com>
2025-03-06 14:49:16 +0800

    pronounce App 1.1

---
+++ qrReader/index.js
@@ -0,0 +1,43 @@
+const wrapper = document.querySelector(".wrapper");
+const form = document.querySelector("form");
+const fileInp = document.querySelector("input");
+const infoText = document.querySelector("p");
+const closeBtn = document.querySelector(".close");
+const copyBtn = document.querySelector(".copy");
+
+function fetchRequest(file, formData) {
+  infoText.innerText = "掃描中...";
+  fetch("https://api.qrserver.com/v1/read-qr-code/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      result = result[0].symbol[0].data;
+      infoText.innerText = result ? "掃描成功" : "掃描失敗";
+      if (!result) return;
+      document.querySelector("textarea").innerText = result;
+      form.querySelector("img").src = URL.createObjectURL(file);
+      wrapper.classList.add("active");
+    })
+    .catch(() => {
+      infoText.innerText = "掃描失敗";
+    });
+}
+
+fileInp.addEventListener("change", async (e) => {
+  let file = e.target.files[0];
+  if (!file) return;
+  let formData = new FormData();
+  formData.append("file", file);
+  fetchRequest(file, formData);
+});
+
+copyBtn.addEventListener("click", () => {
+  let text = document.querySelector("textarea").textContent;
+  navigator.clipboard.writeText(text);
+});
+
+form.addEventListener("click", () => fileInp.click());
+
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
